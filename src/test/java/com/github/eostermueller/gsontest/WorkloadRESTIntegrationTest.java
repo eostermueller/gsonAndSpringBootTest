864d468cb4444301bec47af03da113a18e4e657c
Erik Ostermueller <eostermueller@gmail.com>
2019-07-21 15:55:55 -0500

    using SpringBootTest instead of @JsonTest

--- src/test/java/com/github/eostermueller/gsontest/WorkloadRESTIntegrationTest.java
+++ src/test/java/com/github/eostermueller/gsontest/WorkloadRESTIntegrationTest.java
@@ -10,9 +10,12 @@ import java.net.URI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,7 +26,10 @@ import org.springframework.web.util.UriComponentsBuilder;
  * @author erikostermueller
  *
  */
+//@JsonTest
 @SpringBootTest(classes = com.github.eostermueller.gsontest.WorkloadController.class)
+//@WebMvcTest(controllers = com.github.eostermueller.gsontest.WorkloadController.class)
+//@ContextConfiguration( initializers = com.github.eostermueller.gsontest.WorkloadController.class )
 public class WorkloadRESTIntegrationTest {
 
     @Autowired
